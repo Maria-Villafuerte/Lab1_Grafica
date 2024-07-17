f94092bcadca090488dc64faa2050db821992421
Maria-Villafuerte <villafuerte.mariajose046@gmail.com>
2024-07-17 13:23:53 -0600

    poligon_4

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
     let yellow = Color::new(255, 255, 0);
     let blue = Color::new(0, 0, 255);
     let red = Color::new(255, 0, 0);
+    let green = Color::new(0, 255, 0);
 
     let mut fb = Framebuffer::new(800, 600);
 
@@ -48,13 +49,45 @@ fn main() {
         Vec3::new(436.0, 249.0, 0.0),
     ];
 
+    // Polígono 4
+    let polygon4 = vec![
+        Vec3::new(413.0, 177.0, 0.0),
+        Vec3::new(448.0, 159.0, 0.0),
+        Vec3::new(502.0, 88.0, 0.0),
+        Vec3::new(553.0, 53.0, 0.0),
+        Vec3::new(535.0, 36.0, 0.0),
+        Vec3::new(676.0, 37.0, 0.0),
+        Vec3::new(660.0, 52.0, 0.0),
+        Vec3::new(750.0, 145.0, 0.0),
+        Vec3::new(761.0, 179.0, 0.0),
+        Vec3::new(672.0, 192.0, 0.0),
+        Vec3::new(659.0, 214.0, 0.0),
+        Vec3::new(615.0, 214.0, 0.0),
+        Vec3::new(632.0, 230.0, 0.0),
+        Vec3::new(580.0, 230.0, 0.0),
+        Vec3::new(597.0, 215.0, 0.0),
+        Vec3::new(552.0, 214.0, 0.0),
+        Vec3::new(517.0, 144.0, 0.0),
+        Vec3::new(466.0, 180.0, 0.0),
+    ];
+
+    // Polígono 5 (agujero dentro del polígono 4)
+    let polygon5 = vec![
+        Vec3::new(682.0, 175.0, 0.0),
+        Vec3::new(708.0, 120.0, 0.0),
+        Vec3::new(735.0, 148.0, 0.0),
+        Vec3::new(739.0, 170.0, 0.0),
+    ];
+
     draw_polygon(&mut fb, &polygon1, &[], yellow, white);
     draw_polygon(&mut fb, &polygon2, &[], blue, white);
     draw_polygon(&mut fb, &polygon3, &[], red, white);
+    draw_polygon(&mut fb, &polygon4, &[polygon5], green, white);
 
     fb.render_buffer("out.bmp").unwrap();
     println!("Framebuffer rendered to out.bmp");
 }
+
 
 
 
